2582272c0e328bfe02bd42358c5f95f4e162f28b
bellus869 <b.stepankova03@gmail.com>
2025-09-08 10:14:14 +0200

    translated find.cpp

--- src/find.cpp
+++ src/find.cpp
@@ -1516,9 +1516,9 @@ BOOL AddFoundItem(const char* path, const char* name, DWORD sizeLow, DWORD sizeH
 
 /*
 dirStack' is used to store the directory for later grep. Otherwise, a recursive search in subdirectories would occur while searching the current directory. This approach will first find all files and directories that match the criteria, and then this function will be called for all found directories.
-'dirStack' is only inflated. If items are deleted from it,
+// 'dirStack' stores directories for late grepping. Otherwise, 
 they are only destroyed, but not removed from the array. Therefore, the 'dirStackCount' variable is passed, which contains the actual number of items in the array (always less than or equal to dirStack->Count).
-If there is insufficient memory or subdirectories are not searched,
+// during searching in the current directory, recursive searching in subdirectories would occur. With this
 'dirStack' is equal to NULL.
 If 'duplicateCandidates' != NULL, found items will be added to this array instead of data->FoundFilesListView
 */
@@ -1945,7 +1945,7 @@ unsigned GrepThreadFBody(void* ptr)
                 {
                     dirStack = new TDirectArray<char*>(1000, 1000);
                     if (dirStack == NULL)
-                        TRACE_E(LOW_MEMORY); // the algorithm will run even without the stack
+                    TRACE_E(LOW_MEMORY); // the algorithm will run even without the stack
                 }
 
                 // create a local copy of the ignore list since it has to be processed anyway
